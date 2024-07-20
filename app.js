f5d21276f99c4f8c3e5e6d114a48851e2e07b510
Katsuya Endoh <studio@enkatsu.org>
2024-07-20 23:35:50 +0900

    add forever

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const WebSocketServer = require('ws').WebSocketServer
 const serverOption = isHttps ? {
     cert: readFileSync('/path/to/cert.pem'),
     key: readFileSync('/path/to/key.pem'),
-} : {
-}
+} : { }
 
 const server = createServer(serverOption)
 const wss = new WebSocketServer({ server })
 
 wss.on('connection', function connection(ws) {
+    console.log(`client count: ${wss.clients.size}`)
     ws.on('error', console.error)
     ws.on('message', function message(data) {
         console.log(data.toString('UTF-8'))
